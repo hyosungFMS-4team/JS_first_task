ac6fb5b9f12ede353568dbbdb661b47010b12f02
epass1123 <404.notfound@kakao.com>
2024-04-17 11:26:38 +0900

    feat: add marker text and full map

--- KAKAO_MAP(Geolocation)/main.js
+++ KAKAO_MAP(Geolocation)/main.js
@@ -3,81 +3,93 @@ const carousels = document.getElementById('carousel-lists');
 
 // MAIN
 (async () => {
-    // 
-    buildCarousels();
+  //
+  buildCarousels();
 
-    const map = loadKakaoMap(mapContainer, 37.2526, 127.0723);
+  const map = loadKakaoMap(mapContainer, 37.2526, 127.0723);
 
-    // 좌표
-    const curCoord = await getCoords();
-    const destCoord = {
-        latitude: 37.2526,
-        longtitude: 127.0723
-    };
-    const coords = [curCoord, destCoord];
+  // 좌표
+  const curCoord = await getCoords();
+  const destCoord = {
+    latitude: 37.2526,
+    longtitude: 127.0723,
+  };
+  const coords = [curCoord, destCoord];
 
-    // 좌표 마커로 지도에 추가
-    makeMarkersToMap(map, coords);
+  // 좌표 마커로 지도에 추가
+  makeMarkersToMap(map, coords);
 
-    // 경로 생성 및 지도에 추가
-    const carDirection = await getCarDirection(curCoord, destCoord);
-    console.log(carDirection);
-    makePathLineToMap(map, carDirection);
-    
+  // 경로 생성 및 지도에 추가
+  const carDirection = await getCarDirection(curCoord, destCoord);
+  console.log(carDirection);
+  makePathLineToMap(map, carDirection);
 })();
 
 function makeMarkersToMap(map, coords) {
-    // 모든 마커가 한 번에 표시될 수 있도록 지도 경계 설정
-    const mapBounds = new kakao.maps.LatLngBounds();
-    let imageSrc = './image/pixil-frame-0.png', // 마커이미지의 주소입니다
+  // 모든 마커가 한 번에 표시될 수 있도록 지도 경계 설정
+  const mapBounds = new kakao.maps.LatLngBounds();
+  let imageSrc = './image/pixil-frame-0.png', // 마커이미지의 주소입니다
     imageSize = new kakao.maps.Size(26, 38), // 마커이미지의 크기입니다
     imageOption = { offset: new kakao.maps.Point(13, 38) }; //마커 위치 내부 좌표
-    let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-    for (const coord of coords) {
-        const point = new kakao.maps.LatLng(coord.latitude, coord.longtitude);
-        const marker = new kakao.maps.Marker({ position: point,image: markerImage }  );
+  let content = `<div>
+       <span class="custom-overlay">김기정</span>
+    </div>`;
+  // 커스텀 오버레이를 생성합니다
 
-        mapBounds.extend(point);
-        marker.setMap(map);
-    }
+  let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+  for (const coord of coords) {
+    const point = new kakao.maps.LatLng(coord.latitude, coord.longtitude);
+    const marker = new kakao.maps.Marker({ position: point, image: markerImage });
 
-    map.setBounds(mapBounds);
+    mapBounds.extend(point);
+    marker.setMap(map);
+
+    const customOverlay = new kakao.maps.CustomOverlay({
+      map: map,
+      position: point,
+      content: content,
+      yAnchor: 2.7,
+    });
+    customOverlay.setMap(map);
+  }
+
+  map.setBounds(mapBounds);
 }
 
 function makePathLineToMap(map, direction) {
-    const linePath = [];
-    direction.routes[0].sections[0].roads.forEach(router => {
-        router.vertexes.forEach((_, index) => {
-            if (index % 2 === 0) {
-                linePath.push(new kakao.maps.LatLng(router.vertexes[index + 1], router.vertexes[index]));
-            }
-        });
+  const linePath = [];
+  direction.routes[0].sections[0].roads.forEach(router => {
+    router.vertexes.forEach((_, index) => {
+      if (index % 2 === 0) {
+        linePath.push(new kakao.maps.LatLng(router.vertexes[index + 1], router.vertexes[index]));
+      }
     });
+  });
 
-    // 경로 지도에 추가
-    const polyline = new kakao.maps.Polyline({
-        path: linePath,
-        strokeWeight: 5,
-        strokeColor: '#000000',
-        strokeOpacity: 0.7,
-        strokeStyle: 'solid',
-    });
+  // 경로 지도에 추가
+  const polyline = new kakao.maps.Polyline({
+    path: linePath,
+    strokeWeight: 5,
+    strokeColor: '#000000',
+    strokeOpacity: 0.7,
+    strokeStyle: 'solid',
+  });
 
-    polyline.setMap(map);
+  polyline.setMap(map);
 }
 
 function buildCarousels() {
-    const tasks = localStorage.getItem('tasks');
-    const answerSheet = localStorage.getItem('answerSheet');
-    const score = localStorage.getItem('score');
+  const tasks = localStorage.getItem('tasks');
+  const answerSheet = localStorage.getItem('answerSheet');
+  const score = localStorage.getItem('score');
 
-    JSON.parse(tasks).forEach(task => appendTaskToCarousels(task, 'bg-green', 'bg-blue'));
+  JSON.parse(tasks).forEach(task => appendTaskToCarousels(task, 'bg-green', 'bg-blue'));
 }
 
 function appendTaskToCarousels(task, flipBgColor, defaultBgColor) {
-    const carouselItem = document.createElement('label');
-    carouselItem.setAttribute('class', 'swap swap-flip carousel-item');
-    carouselItem.innerHTML = `
+  const carouselItem = document.createElement('label');
+  carouselItem.setAttribute('class', 'swap swap-flip carousel-item');
+  carouselItem.innerHTML = `
         <input type="checkbox" />
 
         <div class="swap-on swap-container">
@@ -115,61 +127,61 @@ function appendTaskToCarousels(task, flipBgColor, defaultBgColor) {
             </div>
         </div>
         </div>`;
-    carousels.appendChild(carouselItem);
+  carousels.appendChild(carouselItem);
 }
 
 function loadKakaoMap(mapContainer, lat, lng) {
-    const mapOptions = {
-        center: new kakao.maps.LatLng(lat, lng),
-        level: 3,
-    };
-    return new kakao.maps.Map(mapContainer, mapOptions);
+  const mapOptions = {
+    center: new kakao.maps.LatLng(lat, lng),
+    level: 3,
+  };
+  return new kakao.maps.Map(mapContainer, mapOptions);
 }
 function makeMarker(lat, lng) {
-    const markerPos = new kakao.maps.LatLng(lat, lng);
-    const marker = new kakao.maps.Marker({
-        position: markerPos,
-    });
-    return marker;
+  const markerPos = new kakao.maps.LatLng(lat, lng);
+  const marker = new kakao.maps.Marker({
+    position: markerPos,
+  });
+  return marker;
 }
 
 async function getCoords() {
-    const pos = await new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject);
-    });
-    return {
-        longtitude: pos.coords.longitude,
-        latitude: pos.coords.latitude,
-    };
+  const pos = await new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+  return {
+    longtitude: pos.coords.longitude,
+    latitude: pos.coords.latitude,
+  };
 }
 async function getCarDirection(start, end) {
-    const url = 'https://apis-navi.kakaomobility.com/v1/directions';
-    const REST_API_KEY = 'e639f9820bd9dfd6a0627ecb6b06f5f3';
+  const url = 'https://apis-navi.kakaomobility.com/v1/directions';
+  const REST_API_KEY = 'e639f9820bd9dfd6a0627ecb6b06f5f3';
 
-    const origin = `${start.longtitude},${start.latitude}`;
-    const destination = `${end.longtitude},${end.latitude}`;
-    const queryParams = new URLSearchParams({
-        origin: origin,
-        destination: destination,
+  const origin = `${start.longtitude},${start.latitude}`;
+  const destination = `${end.longtitude},${end.latitude}`;
+  const queryParams = new URLSearchParams({
+    origin: origin,
+    destination: destination,
+  });
+
+  const headers = {
+    Authorization: `KakaoAK ${REST_API_KEY}`,
+    'Content-Type': 'application/json',
+  };
+
+  const requestUrl = `${url}?${queryParams}`;
+
+  try {
+    const response = await fetch(requestUrl, {
+      method: 'GET',
+      headers: headers,
     });
-
-    const headers = {
-        Authorization: `KakaoAK ${REST_API_KEY}`,
-        'Content-Type': 'application/json',
-    };
-    
-    const requestUrl = `${url}?${queryParams}`;
-
-    try {
-        const response = await fetch(requestUrl, {
-            method: 'GET',
-            headers: headers,
-        });
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-    } catch (error) {
-        console.error('Error:', error);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
     }
+    return response.json();
+  } catch (error) {
+    console.error('Error:', error);
+  }
 }
